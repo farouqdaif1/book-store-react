c02b3de4b3d76e9d48c1d9913e6acf00a2ef82cf
farouqdaif1 <daiffarouq@gmail.com>
2021-12-18 00:49:04 +0200

    Fix html and css

--- src/components/bookComponents/renderbook.js
+++ src/components/bookComponents/renderbook.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-
+import { CircularProgressbar } from 'react-circular-progressbar';
 import PropTypes from 'prop-types';
 import { removeBook } from '../../redux/books/books';
 
 const Book = (props) => {
   const { book } = props;
+  const percnt = 33;
   const dispatch = useDispatch();
   return (
     <li id={book.item_id} className="book-sec">
@@ -16,13 +17,32 @@ const Book = (props) => {
         <span className="book-name">
           {` ${book.title} `}
         </span>
+        <div className="btn-sec">
+          <button type="button" className="btn-c"> Comments </button>
+          <div className="line-2" />
+          <button type="submit" className="remove-btn btn-r" onClick={() => dispatch(removeBook(book))}>REMOVE</button>
+          <div className="line-2" />
+          <button type="button" className="btn-e"> Edit </button>
+        </div>
       </div>
-      <div className="btn-sec">
-        <button type="button" className="btn-c"> Comments </button>
-        <div className="line-2" />
-        <button type="submit" className="remove-btn btn-r" onClick={() => dispatch(removeBook(book))}>REMOVE</button>
-        <div className="line-2" />
-        <button type="button" className="btn-e"> Edit </button>
+
+      <div className="col-2">
+        <div className="prog-pie">
+          <CircularProgressbar value={percnt} />
+        </div>
+        <div className="prog-info">
+          <h2 className="prog-num">
+            {percnt}
+            %
+          </h2>
+          <p className="prog-comp">Completed</p>
+        </div>
+      </div>
+
+      <div className="col-3">
+        <p className="chap-curr">CURRENT CHAPTER</p>
+        <p className="chap">Chapter 13</p>
+        <button type="submit" className="update-btn">UPDATE PROGRESS</button>
       </div>
     </li>
   );
